3e331435d3f19edf9cf8526308315af8cf610841
Jmcdermott7001 <jpm9rb@mail.umkc.edu>
2015-10-04 02:10:37 -0500

    Create Term.cpp
    This is just a draft of the Term.cpp still needs a lot of work and correcting

---
+++ Term.cpp
@@ -0,0 +1,47 @@
+#include "Term.h"
+#include <iostream>
+
+using namespace std;
+
+Term::Term(){
+
+    coefficient = 0; // not sure if default constructor is an empty constructor or if we setting them to 0
+    exponent = 0;
+}
+Term::Term(int t_coeff, int t_exp){
+
+    coefficient = t_coeff;
+    exponent = t_exp;
+}
+Term::Term(Term& a_Term){
+
+    coefficient = a_Term.coefficient;
+    exponent = a_Term.exponent;
+}
+bool Term::operator > (Term& other_Term)
+{
+    return exponent < other_Term.exponent;
+}
+bool Term::operator ==(Term& other_Term){
+
+    return exponent == other_Term.exponent;
+}
+istream& operator >>(istream& INtermStream, Term& a_Term){
+
+    char char1, varLetter, carrot;
+    if (isdigit(char1))
+    INtermStream >> a_Term.coefficient;
+    INtermStream >> varLetter;
+    INtermStream >> a_Term.exponent;
+    INtermStream >> carrot;
+    return INtermStream;        
+}
+
+ostream& operator <<(ostream& OUTtermStream, const Term& a_Term){   
+
+    OUTtermStream << a_Term.coefficient;
+    OUTtermStream << "X";
+    OUTtermStream << a_Term.exponent;
+    OUTtermStream << "^";
+    return OUTtermStream;
+}
