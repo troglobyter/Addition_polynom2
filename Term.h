516ddad0fa7db1771b431277c2b45d9f300f1030
unknown <jijgy9@KC-SCE364LAB-10.kc.umkc.edu>
2015-10-06 19:17:00 -0500

    no big changes to Term.h

--- Term.h
+++ Term.h
@@ -8,15 +8,16 @@ using namespace std;
 
 class Term{
 private:
-  int coefficient;
-  int exponent;
+	int coefficient;
+	int exponent;
 public:
-  Term();
-  Term(int t_coeff, int t_exp);
-  Term(Term& a_Term);
-  bool negative_term() const; 
-  bool operator >(Term& other_Term);
-  bool operator ==(Term& other_Term);
-  friend istream& operator >>(istream& INtermStream, Term& a_Term);
-  friend ostream& operator <<(ostream& OUTtermStream, const Term& a_Term);
+	friend class Term_list;
+	Term();
+	Term(int t_coeff, int t_exp);
+	Term(const Term& a_Term);
+	bool negative_term() const; 
+	bool operator >(Term& other_Term);
+	bool operator ==(Term& other_Term);
+	friend istream& operator >>(istream& INtermStream, Term& a_Term);
+	friend ostream& operator <<(ostream& OUTtermStream, const Term& a_Term);
 };
