6d3fbb24c4f587d3f151f98a217d12cd9e01ba11
unknown <jijgy9@KC-SCE364LAB-10.kc.umkc.edu>
2015-10-06 19:17:47 -0500

    some different use of new implement of a list::iterator as member variable.

--- Term_list.cpp
+++ Term_list.cpp
@@ -3,53 +3,63 @@
 Term_list::Term_list():list<Term>(){
 }
 
-Term_list::Term_list(Term_list& other_polynomial){
+Term_list::Term_list(const Term_list& other_polynomial){
 	list<Term>::const_iterator c_itr;
 	for(c_itr = other_polynomial.begin(); c_itr != other_polynomial.end(); ++c_itr){
 		this->push_back(*c_itr);
 	}
 }
 
-void Term_list::selection_sort(list<Term>::iterator itr1){
-	if(itr1 +1 == this->end()){
+void Term_list::selection_sort(){
+	++the_itr;
+	if(the_itr == this->end()){
 		return;
 	}
-	Term max_Term = *itr1;
-	list<Term>::iterator max_itr_pos = itr1;
-	for(list<Term>::iterator itr2 = itr1 +1; itr2 != this->end(); ++itr2){
+	--the_itr;
+	Term max_Term = *the_itr;
+	list<Term>::iterator max_itr_pos = the_itr;
+	for(list<Term>::iterator itr2 = ++the_itr; itr2 != this->end(); ++itr2){
 		if(*itr2 > max_Term)
 			max_itr_pos = itr2;
 	}
-	swap(*itr1, *max_itr_pos);
-	++itr1;
-	selection_sort(itr1);
+	--the_itr;
+	Term current_front_term = *the_itr;
+	Term current_max_term = *max_itr_pos;
+	Term temp_term;
+	temp_term = current_front_term;
+	current_front_term = current_max_term;
+	current_max_term = temp_term;
+
+	++the_itr;
+	selection_sort();
 }
 
-void Term_list::collect_like_terms(list<Term>::iterator itr1){
-	list<Term>::iterator itr2 = itr1 +1;
+void Term_list::collect_like_terms(){
+	list<Term>::iterator itr2 = ++the_itr;
+	--the_itr;
 	if(itr2 == this->end())
 		return;
-	while(*itr1 == *itr2){
-		itr1->coefficient += itr2->coefficient;
+	while(*the_itr == *itr2){
+		the_itr->coefficient += itr2->coefficient;
 		itr2 = this->erase(itr2);
 	}
-	itr1 = itr2;
-	collect_like_terms(itr1);
+	the_itr = itr2;
+	collect_like_terms();
 }
 
-istream& Term_list::operator >>(istream& INpolyStream, Term_list& polynomial){
+istream& operator >>(istream& INpolyStream, Term_list& polynomial){
 	Term Term_i;
 	while(INpolyStream >> Term_i){
 		polynomial.push_back(Term_i);
 	}
-	list<Term>::iterator itr = polynomial.begin();
-	polynomial.selection_sort(itr);
-	itr = polynomial.begin();
-	polynomial.collect_like_terms(itr);
+	polynomial.the_itr = polynomial.begin();
+	polynomial.selection_sort();
+	polynomial.the_itr = polynomial.begin();
+	polynomial.collect_like_terms();
 	return INpolyStream;
 }
 
-ostream& Term_list::operator <<(ostream& OUTpolyStream, Term_list& polynomial){
+ostream& operator <<(ostream& OUTpolyStream, const Term_list& polynomial){
 	if(polynomial.empty())
 		throw std::invalid_argument("The Term_list object is empty.");
 
@@ -58,11 +68,13 @@ ostream& Term_list::operator <<(ostream& OUTpolyStream, Term_list& polynomial){
 		OUTpolyStream << '-';
 	for(c_itr = polynomial.begin(); c_itr != polynomial.end(); ++c_itr){
 		OUTpolyStream << *c_itr;
-		if((c_itr +1) != polynomial.end()){
-			if(c_itr->negative_term())
+		if ((++c_itr) != polynomial.end()){
+			if (c_itr->negative_term())
 				OUTpolyStream << " - ";
 			else
-				OUTpolyStream << " + "; 
+				OUTpolyStream << " + ";
+		}
+		--c_itr;
 	}
 	return OUTpolyStream;
 }
@@ -74,9 +86,9 @@ Term_list& Term_list::operator +(const Term_list& other_polynomial) const{
 		result_polynomial.push_back(*c_itr);
 	}
 	list<Term>::iterator itr;
-	itr = result_polynomial.begin();
-	result_polynomial.selection_sort(itr);
-	itr = result_polynomial.begin();
-	result_polynomial.collect_like_terms(itr);
+	result_polynomial.the_itr = result_polynomial.begin();
+	result_polynomial.selection_sort();
+	result_polynomial.the_itr = result_polynomial.begin();
+	result_polynomial.collect_like_terms();
 	return result_polynomial;
 } 
